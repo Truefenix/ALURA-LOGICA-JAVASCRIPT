d5a2dc81cfd3870cd4453f6c6d44449240ea56df
fenix <fenix.azul09@protonmail.com>
2022-12-28 17:46:02 -0300

    pasta outros

---
+++ outros/BloquearCharEspecial.js
@@ -0,0 +1,15 @@
+function containsSpecialChars(str) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
+  }
+  
+  console.log(containsSpecialChars('hello!')); // 👉️ true
+  console.log(containsSpecialChars('abc')); // 👉️ false
+  console.log(containsSpecialChars('one two')); // 👉️ false
+  
+  if (containsSpecialChars('hello!')) {
+    console.log('✅ string contains special characters');
+  } else {
+    console.log('⛔️ string does NOT contain special characters');
+  }
+  
